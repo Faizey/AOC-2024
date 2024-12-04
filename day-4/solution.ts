dcf73442f08a666fd0e48ee1d689607349e3d8f0
Faizey <shaun_miah08@hotmail.co.uk>
2024-12-04 23:55:26 +0000

    AOC typescript day 4 part 2

--- day-4/solution.ts
+++ day-4/solution.ts
@@ -36,7 +36,7 @@ export function part_1(input: string) {
 	const matrix = input.replace("\r", "").split("\n");
 	console.log(matrix.length);
 	const word = "XMAS";
-	let matchedCount = 0;
+	let answer = 0;
 
 	const dx = [-1, 0, 1, 1, 1, 0, -1, -1];
 	const dy = [-1, -1, -1, 0, 1, 1, 1, 0];
@@ -45,13 +45,53 @@ export function part_1(input: string) {
 		for (let x = 0; x < matrix[y].length; x++) {
 			for (let dir = 0; dir < dx.length; dir++) {
 				if (findWord(matrix, word, x, y, dx[dir], dy[dir])) {
-					matchedCount += 1;
+					answer += 1;
 				}
 			}
 		}
 	}
 
-	return matchedCount;
+	return answer;
 }
 
-export function part_2(input: string) {}
+export function part_2(input: string) {
+	const matrix = input.replace("\r", "").split("\n");
+	let answer = 0;
+
+	// Start on second row and finish on second to last row
+	for (let y = 1; y < matrix.length - 1; y++) {
+		for (let x = 0; x < matrix[y].length; x++) {
+			let count = 0;
+			const l = matrix[y][x];
+
+			if (l === "A") {
+				const topLeftLetter = matrix[y - 1][x - 1];
+				const topRightLetter = matrix[y - 1][x + 1];
+				const bottomLeftLetter = matrix[y + 1][x - 1];
+				const bottomRightLetter = matrix[y + 1][x + 1];
+
+				if (topLeftLetter === "M" && bottomRightLetter === "S") {
+					count += 1;
+				}
+
+				if (topRightLetter === "M" && bottomLeftLetter === "S") {
+					count += 1;
+				}
+
+				if (bottomRightLetter === "M" && topLeftLetter === "S") {
+					count += 1;
+				}
+
+				if (bottomLeftLetter === "M" && topRightLetter === "S") {
+					count += 1;
+				}
+			}
+
+			if (count === 2) {
+				answer += 1;
+			}
+		}
+	}
+
+	return answer;
+}
