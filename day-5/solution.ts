4557b7dd80aadf184726857f5ddc88d2699c39f0
Faizey <shaun_miah08@hotmail.co.uk>
2024-12-05 13:13:55 +0000

    AOC typescript day 5 part 2

--- day-5/solution.ts
+++ day-5/solution.ts
@@ -44,6 +44,75 @@ function part_1(input: string) {
 	return answer;
 }
 
-function part_2(input: string) {}
+function part_2(input: string) {
+	const rules: number[][] = [];
+	const allPages: number[][] = [];
+	const incorrectPages: number[][] = [];
+	let answer = 0;
+
+	input.split("\n").forEach((l) => {
+		if (!l) return;
+
+		if (l.includes("|")) {
+			rules.push(l.split("|").map(Number));
+			return;
+		}
+
+		allPages.push(l.split(",").map(Number));
+	});
+
+	allPages.forEach((pages) => {
+		let skip = false;
+
+		rules.forEach((rule) => {
+			if (skip) return;
+
+			const [before, after] = rule;
+			const pageBeforeIndex = pages.indexOf(before);
+			const pageAfterIndex = pages.indexOf(after);
+
+			if (pageAfterIndex === -1 || pageBeforeIndex === -1) {
+				return;
+			}
+
+			if (pageBeforeIndex > pageAfterIndex) {
+				incorrectPages.push(pages);
+				skip = true;
+				return;
+			}
+		});
+	});
+
+	incorrectPages.forEach((pages) => {
+		let keepTrying = true;
+		while (keepTrying) {
+			let hasSorted = true;
+			rules.forEach((rule) => {
+				const [before, after] = rule;
+				const pageBeforeIndex = pages.indexOf(before);
+				const pageAfterIndex = pages.indexOf(after);
+
+				if (pageAfterIndex === -1 || pageBeforeIndex === -1) {
+					return;
+				}
+
+				if (pageBeforeIndex > pageAfterIndex) {
+					pages[pageAfterIndex] = before;
+					pages[pageBeforeIndex] = after;
+					hasSorted = false;
+					return;
+				}
+			});
+
+			if (hasSorted) {
+				keepTrying = false;
+			}
+		}
+
+		answer += pages[Math.floor(pages.length / 2)];
+	});
+
+	return answer;
+}
 
 export { part_1, part_2 };
