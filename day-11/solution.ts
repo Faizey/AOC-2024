8e8e6e718c689a5ea2b55b2e5e8b72c02d2ab217
Faizey <shaun_miah08@hotmail.co.uk>
2024-12-12 23:36:40 +0000

    day-11 optimisations

--- day-11/solution.ts
+++ day-11/solution.ts
@@ -1,34 +1,34 @@
 function part_1(input: string) {
-	let blocks = input.split(" ");
+	let blocks = input.split(" ").map(Number);
 	const blinks = 25;
 
 	for (let count = 0; count < blinks; count++) {
+		let newBlocks = [];
+
 		for (let i = 0; i < blocks.length; i++) {
 			const block = blocks[i];
 
-			if (parseInt(block, 10) === 0) {
-				blocks[i] = "1";
+			if (block === 0) {
+				newBlocks.push(1);
 				continue;
 			}
 
-			if (block.length % 2 === 0) {
-				blocks.splice(
-					i++,
-					1,
-					"" + parseInt(block.substring(0, block.length / 2), 10),
-					"" + parseInt(block.substring(block.length / 2, block.length), 10),
+			if (block.toString().length % 2 === 0) {
+				const strBlock = block.toString();
+				newBlocks.push(
+					parseInt(strBlock.substring(0, strBlock.length / 2), 10),
+					parseInt(strBlock.substring(strBlock.length / 2), 10),
 				);
-
-				continue;
+			} else {
+				newBlocks.push(block * 2024);
 			}
-
-			blocks[i] = "" + parseInt(block, 10) * 2024;
 		}
+
+		blocks = newBlocks;
 	}
 
 	return blocks.length;
 }
-
 function part_2(input: string) {}
 
 export { part_1, part_2 };
