cb4a19f23c37f03bfcd10993122978a8b0956dbe
Faizey <shaun_miah08@hotmail.co.uk>
2024-12-14 22:02:59 +0000

    AOC typescript day 11 part 2

--- day-11/solution.ts
+++ day-11/solution.ts
@@ -1,49 +1,64 @@
-function blink(blocks) {
-	let newBlocks = [];
+function parse(input: string) {
+	const stones = input.split(" ").map(BigInt);
+	const stones_map = new Map<bigint, number>();
 
-	for (let i = 0; i < blocks.length; i++) {
-		const block = blocks[i];
+	for (const stone of stones) {
+		stones_map.set(stone, (stones_map.get(stone) ?? 0) + 1);
+	}
 
-		if (block === 0) {
-			newBlocks.push(1);
+	return stones_map;
+}
+
+function add(stones: Map<bigint, number>, stone: bigint, count: number) {
+	stones.set(stone, (stones.get(stone) ?? 0) + count);
+}
+
+function blink(stones: Map<bigint, number>) {
+	const next = new Map<bigint, number>();
+
+	for (const [stone, count] of stones) {
+		if (stone === 0n) {
+			add(next, 1n, count);
 			continue;
 		}
 
-		if (block.toString().length % 2 === 0) {
-			const strBlock = block.toString();
-			newBlocks.push(
-				parseInt(strBlock.substring(0, strBlock.length / 2), 10),
-				parseInt(strBlock.substring(strBlock.length / 2), 10),
-			);
-		} else {
-			newBlocks.push(block * 2024);
+		const as_string = stone.toString();
+
+		if (as_string.length % 2 === 0) {
+			const left = BigInt(as_string.substring(0, as_string.length / 2));
+			const right = BigInt(as_string.substring(as_string.length / 2));
+			add(next, left, count);
+			add(next, right, count);
+			continue;
 		}
+
+		add(next, stone * 2024n, count);
 	}
 
-	return newBlocks;
+	console.log(next)
+
+	return next;
 }
 
 function part_1(input: string) {
-	let blocks = input.split(" ").map(Number);
+	let stones = parse(input);
 	const blinks = 25;
 
-	for (let count = 0; count < blinks; count++) {
-		blocks = blink(blocks);
+	for (let i = 0; i < blinks; i++) {
+		stones = blink(stones);
 	}
 
-	return blocks.length;
+	return [...stones].reduce((acc, [, count]) => acc + count, 0);
 }
 
 function part_2(input: string) {
-	let blocks = input.split(" ").map(Number);
-	const blinks = 50;
+	let stones = parse(input);
+	const blinks = 75;
 
-	for (let count = 0; count < blinks; count++) {
-		blocks = blink(blocks);
-		console.log(blocks.length)
+	for (let i = 0; i < blinks; i++) {
+		stones = blink(stones);
 	}
 
-	return blocks.length;
+	return [...stones].reduce((acc, [, count]) => acc + count, 0);
 }
-
 export { part_1, part_2 };
